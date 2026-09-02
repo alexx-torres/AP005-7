5fec432d4ed7425d93e1369e712d1557069a55aa
alexx-torres <alexandertorreslugo@gmail.com>
2026-09-02 10:47:20 -0500

    Update dictionaries.py
    Updated comments to clarify the purpose of the dictionary and expected output.

--- Corte1/tarea3/dictionaries.py
+++ Corte1/tarea3/dictionaries.py
@@ -1,5 +1,5 @@
-building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3} """ nos dan el nombre de un diccionario llamado 
+"building_heights" en el cual los nombres clave son de edificaciones y el valor de dichas claves es su altura en metros. """
 print(building_heights["Burj Khalifa"])
 print(building_heights["Ping An"])
-# al inicio del código nos dan el nombre de un diccionario llamado "building_heights" en el cual el cual los nombres clave son de edificaciones y el valor de dichas claves es su altura en metros.
-# luego para corroborar que el código esté bien, pedimos que imprima el valor de algunos nombres clave.
+# luego para corroborar que el código esté bien, pedimos que imprima el valor de algunos nombres clave y debería darnos los valores 828 y 599
